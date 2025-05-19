0dae49e8180e7dc49f8c3af560fac8d043feb0d5
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrietaa@gmail.com>
2025-05-19 12:30:59 -0500

    feat: se definió una clase que utilice las funcionalidades de la libreria AOP para separar del codigo los requerimientos no funcionales de registrar log para hacer trazabilidad

---
+++ src/main/java/com/caribedev/TaskManager/Common/Loggin/LogginAspect.java
@@ -0,0 +1,76 @@
+package com.caribedev.TaskManager.Common.Loggin;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Before;
+
+
+/**
+ *
+ * @author John Carlos Arrieta Arrieta
+ */
+
+
+@Component
+@Aspect
+@Slf4j  
+public class LogginAspect {
+   
+//    private final static Logger logger = LoggerFactory.getLogger(LogginAspect.class);
+    
+    @Before("within(com.carivede.TaskManager.Application.Commands.*)")
+    public void logBeforeCommands(JoinPoint joinPoint){
+        var messageLog = "Before -> "
+                +joinPoint.getSignature().getDeclaringTypeName()
+                +"."+joinPoint.getSignature().getName();
+       log.info(messageLog);
+    }
+    
+    @After(
+            "(within(com.carivede.TaskManager.Presentation.Controllers.*)) ||"
+            + "(within(com.carivede.TaskManager.Presentation.Queries.*))"
+    )
+    public void logAfterControllers(JoinPoint joinPointer){
+        log.info("After-> {}.{}",
+                joinPointer.getSignature().getDeclaringTypeName(),
+                joinPointer.getSignature().getName()
+                
+        );
+    }
+    
+   @Around("within(com.carivede.TaskManager.Infraestructure.Repositories.*)")
+    public long logHandleExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        var start = System.currentTimeMillis();
+        
+        log.info("Start -> {}.{}",
+                proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName()
+        );
+         var end = System.currentTimeMillis() - start;
+         
+         log.info("End -> {}.{}",
+                proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName()
+        );
+         return end;
+    }
+    
+     @AfterThrowing(pointcut = 
+            "within(com.carivede.TaskManager.Infraestructure.Repositories.*)",
+            throwing = "ex"
+    )
+     public void logRepositoryException(JoinPoint joinPoint, Throwable ex){
+         log.info("Exception -> {}.{}: {}",
+                 joinPoint.getSignature().getDeclaringTypeName(),
+                 joinPoint.getSignature().getName(),
+                 ex.getMessage(), ex);
+     }
+    
+}
